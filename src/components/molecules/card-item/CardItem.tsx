fca552e952e5fe42a156b1f11a4924b3b1ae7dee
denis master <dimitriev.denis1988@gmail.com>
2022-08-03 08:37:55 +0300

    finish ProductCard

--- src/components/molecules/card-item/CardItem.tsx
+++ src/components/molecules/card-item/CardItem.tsx
@@ -1,26 +1,27 @@
 import "./CardItem.scss";
+
 import { ReactComponent as FavoritesIcon } from "../../../assets/icons/favorite-heart.svg";
 import { ReactComponent as ShoppingCartIcon } from "../../../assets/icons/shopping-cart.svg";
 import Button from "../../atoms/ui/button/Button";
+import samo from "./samo.png";
 
 export const CardItem = () => {
   return (
     <div className="card-item-container">
       <div className="card-item-img">
-        <img src="" alt="" />
+        <img src={samo} alt="" />
       </div>
       <div className="card-item-body">
-        <div className="body-top">
-          <div className="title">Kugoo Kirin M4</div>
-          <div className="params">
-            <ul className="params-list">
-              <li className="params-list-item">Param 1</li>
-              <li className="params-list-item">Param 2</li>
-              <li className="params-list-item">Param 3</li>
-              <li className="params-list-item">Param 4</li>
-            </ul>
-          </div>
+        <div className="title">Kugoo Kirin M4</div>
+        <div className="params">
+          <ul className="params-list">
+            <li className="params-list-item">Param 1</li>
+            <li className="params-list-item">Param 2</li>
+            <li className="params-list-item">Param 3</li>
+            <li className="params-list-item">Param 4</li>
+          </ul>
         </div>
+
         <div className="body-bottom">
           <div className="price">
             <span className="old-price">50000 MDL</span>
